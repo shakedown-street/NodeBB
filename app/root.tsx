52c8ca7c8da83760e954fdfb8ac224b87adb99cf
jprice <jprice@izeni.com>
2025-12-08 12:14:29 -0700

    Add punkweb-ui

--- app/root.tsx
+++ app/root.tsx
@@ -14,11 +14,15 @@ export const links: Route.LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
+  {
+    rel: 'stylesheet',
+    href: '/punkweb-ui.min.css',
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
