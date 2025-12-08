afd226e7e3fe7b5a70e98946cba53ddc11e78c76
jprice <jprice@izeni.com>
2025-12-08 13:41:17 -0700

    Format and sort imports

--- app/root.tsx
+++ app/root.tsx
@@ -2,8 +2,8 @@ import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration }
 import type { Route } from './+types/root';
 import './app.css';
 import Nav from './components/nav';
-import { getUser } from './services/auth.service';
 import { ThemeProvider } from './context/theme';
+import { getUser } from './services/auth.service';
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
